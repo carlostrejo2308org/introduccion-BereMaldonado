f07a896e5fd36f37257092a7ba704a9ff3b704fc
BereMaldonado <98624071+BereMaldonado@users.noreply.github.com>
2023-08-21 04:54:09 +0000

    Fibo

---
+++ fibo.java
@@ -0,0 +1,18 @@
+package introduccion-BereMaldonado;
+
+public class fibo {
+
+ public static void main (String args[] ){
+			System.out.println(fibonacci(10));
+		}//main
+		
+		
+	static int fibonacci(int n) {
+			if(n>2)
+				return fibonacci(n-1) + fibonacci(n-2);
+			else
+				return 1;
+			
+			 
+		}
+}
